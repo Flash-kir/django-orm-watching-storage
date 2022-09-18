da832bfcdf8ee70a5dbc43e0d0918a2fe2fa97de
Alexey <flash-kir@yandex.ru>
2022-09-18 21:07:01 +0300

    all tasks done

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,19 +1,37 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+import datetime
+from django.shortcuts import get_object_or_404
+
+
+def get_duration(visit):
+    if visit.leaved_at:
+        visit_end_time = visit.leaved_at
+    else:
+        visit_end_time = localtime(timezone('UTC'))
+    return datetime.timedelta(
+        days=visit_end_time.day-visit.entered_at.day,
+        hours=visit_end_time.hour-visit.entered_at.hour,
+        minutes=visit_end_time.minute-visit.entered_at.minute
+    )
+
+
+def if_visit_long(visit, minutes=60):
+    return get_duration(visit) >= datetime.timedelta(minutes=minutes)
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.all()[0]
-    # Программируем здесь
-
-    this_passcard_visits = [
-        {
-            'entered_at': '11-04-2018',
-            'duration': '25:03',
-            'is_strange': False
-        },
-    ]
+    passcard = get_object_or_404(Passcard, passcode=passcode)
+    passcard_visits = Visit.objects.filter(passcard=passcard.id)
+    this_passcard_visits = []
+    for visit in passcard_visits:
+        this_passcard_visits.append({
+            'entered_at': visit.entered_at,
+            'duration': get_duration(visit),
+            'is_strange': if_visit_long(visit)
+        })
+    print(this_passcard_visits)
     context = {
         'passcard': passcard,
         'this_passcard_visits': this_passcard_visits
--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,18 +1,28 @@
 from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
+import datetime
+from pytz import timezone
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    active_visits = Visit.objects.filter(leaved_at__isnull=True)
+    localtime_now = datetime.datetime.now(timezone('UTC'))
+    '''print(localtime_now)
+    print(active_visits[0].entered_at)'''
+    non_closed_visits = []
+    for active_visit in active_visits:
+        entered_at = active_visit.entered_at
+        non_closed_visits.append({
+            'who_entered': active_visit.passcard.owner_name,
+            'entered_at': entered_at,
+            'duration': datetime.timedelta(
+                days=localtime_now.day-entered_at.day,
+                hours=localtime_now.hour-entered_at.hour,
+                minutes=localtime_now.minute-entered_at.minute
+            )
+        })
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
-        }
-    ]
     context = {
         'non_closed_visits': non_closed_visits,  # не закрытые посещения
     }
